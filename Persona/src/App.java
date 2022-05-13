91efbf0421ec416f7350e97f235b073d66fcc17f
Iker Lahera <iker.lahera.tecno@gmail.com>
2022-05-13 12:24:21 +0200

    “Versió 1 del projecte mitjana00”.

--- Persona/src/App.java
+++ Persona/src/App.java
@@ -1,10 +1,10 @@
 public class App {
-    
+  /* esto es un comentario*/   
     /** 
      * @param args
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola, mundo!");
+        System.out.println("Hola, mundo c!");
     }
 }
